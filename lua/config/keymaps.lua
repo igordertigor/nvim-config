8f98c0ac31530345ff43dd3230d4e75968ca5751
Ingo Fruend <github@ingofruend.net>
2025-09-07 21:43:26 +0200

    Only markdown for special character keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-vim.keymap.set("i", "<C-g>o", '“', { desc = "Smart opening quote" })
-vim.keymap.set("i", "<C-g>c", '”', { desc = "Smart closing quote" })
-vim.keymap.set("i", "<C-g>O", "‘", { desc = "Smart opening single quote" })
-vim.keymap.set("i", "<C-g>C", "’", { desc = "Smart closing single quote" })
-vim.keymap.set("i", "<C-g>.", "…", { desc = "Insert ellipsis" })
