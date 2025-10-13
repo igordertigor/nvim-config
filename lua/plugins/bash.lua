a422e774dc181ed2db7a55c805901ee2711f792c
Ingo Fruend <ingo.fruend@workist.com>
2025-10-13 12:11:16 +0200

    williamboman/mason.nvim -> mason-org/mason.nvim

--- lua/plugins/bash.lua
+++ lua/plugins/bash.lua
@@ -1,7 +1,7 @@
 return {
   -- Ensure bash LSP is installed
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "bash-language-server", "shellcheck" })
