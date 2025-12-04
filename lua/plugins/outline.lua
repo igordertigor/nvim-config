61a4a2db3faa3a17731fddbd4d148cf5a5f19ab2
Ingo Fruend <ingo.fruend@workist.com>
2025-12-04 09:41:18 +0100

    Configure outline plugin

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,34 @@
+return {
+  "hedyhli/outline.nvim",
+  opts = {
+    symbols = {
+      filter = {
+        -- TypeScript/TSX symbols
+        "Class",
+        "Constructor",
+        "Enum",
+        "Function",
+        "Interface",
+        "Module",
+        "Method",
+        "Struct",
+        -- Python-specific
+        "Array",
+        "Boolean",
+        "Constant",
+        "EnumMember",
+        "Event",
+        "Field",
+        "Key",
+        "Namespace",
+        "Number",
+        "Object",
+        "Package",
+        "Property",
+        "String",
+        "TypeParameter",
+        "Variable",
+      },
+    },
+  },
+}
