5563fe4b643d964533fe0b59e520dd970048c591
Ingo Fruend <ingo@oudyo.com>
2026-01-04 20:50:51 +0100

    Manage zettel navigation via nvi

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.keymap.set(
+      "n",
+      "<C-]>",
+      require("zettel/links").follow,
+      { buffer = true, desc = "Follow Zettelkasten link" }
+    )
+  end,
+})
+
